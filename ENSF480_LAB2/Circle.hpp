c006cedc95e699165a19a85c56bf2435036281ee
Nathan Godard <nathan.godard1@ucalgary.ca>
2018-09-21 16:34:25 -0600

    Circle and Curvecut exist

---
+++ ENSF480_LAB2/Circle.hpp
@@ -0,0 +1,14 @@
+//
+//  Circle.hpp
+//  ENSF480_LAB2
+//
+//  Created by Nathan Godard on 2018-09-21.
+//  Copyright © 2018 Nathan Godard. All rights reserved.
+//
+
+#ifndef Circle_hpp
+#define Circle_hpp
+
+#include <stdio.h>
+
+#endif /* Circle_hpp */
---
+++ ENSF480_LAB2/CurveCut.hpp
@@ -0,0 +1,14 @@
+//
+//  CurveCut.hpp
+//  ENSF480_LAB2
+//
+//  Created by Nathan Godard on 2018-09-21.
+//  Copyright © 2018 Nathan Godard. All rights reserved.
+//
+
+#ifndef CurveCut_hpp
+#define CurveCut_hpp
+
+#include <stdio.h>
+
+#endif /* CurveCut_hpp */
