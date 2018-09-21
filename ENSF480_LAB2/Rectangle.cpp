302eb1984e141dadcc488e97b183f2881bd0727b
Jathniel Ong <ong.jathniel@gmail.com>
2018-09-21 15:32:24 -0600

    finished rectangle

--- ENSF480_LAB2/Rectangle.cpp
+++ ENSF480_LAB2/Rectangle.cpp
@@ -6,8 +6,29 @@
 //  Copyright © 2018 Nathan Godard. All rights reserved.
 //
 
-//#include "Rectangle.hpp"
-//
-//class Rectangle {
-//    
-//};
+#include <iostream>
+#include "Rectangle.hpp"
+#include <assert.h>
+
+using namespace std;
+
+double Rectangle::area() {
+	assert(side_a >= 0);
+	assert(side_b >= 0);
+	return side_a * side_b;
+}
+
+double Rectangle::perimeter() {
+	assert(side_a >= 0);
+	assert(side_b >= 0);
+	return side_a * 2 + side_b * 2;
+}
+
+void Rectangle::display() {
+	cout << "Rectangle Name: " << this->shapeName << endl;
+	origin.display();
+	cout << "Side a: " << side_a << endl;
+	cout << "Side b: " << side_b <<endl;
+	cout << "Area: " << area() << endl;
+	cout << "Perimeter: " << perimeter() << endl;
+}
--- ENSF480_LAB2/Rectangle.hpp
+++ ENSF480_LAB2/Rectangle.hpp
@@ -12,34 +12,14 @@
 
 
 class Rectangle : public Square {
+protected:
 	double side_b;
-	Rectangle();
 public:
+	Rectangle(double x, double y, double side_a, double side_b, char* shapeName) : Square(x, y, side_a, shapeName), side_b(side_b) {};
 	double area();
 	double perimeter();
-	//void get();
-
 	void display();
+	
 };
-
-/*
-class, in addition to its constructor (no default constructor), should support the following functions:
-- area – that calculates and returns the area of a rectangle
-- perimeter – that calculates and returns the perimeter of a rectangle
-- get and set – that retrieves or changes the values of its private data members.
-- display – that displays the name, and x and y coordinate origin of the shape, side_a, side_b, area,
-and perimeter of a rectangle object, in the following format:
-Rectangle Name:
-X-coordinate:
-Y-coordinate:
-Side a:
-Side b:
- Area:
- Perimeter;
-
-- More Functions, if needed.
-*/
-
-
 
 #endif /* Rectangle_hpp */
